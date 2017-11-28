b15e8839d8929af2a73d1096dc2b0fd9466b3216
unknown <alberto.soutullo@UTWKS0590.liveu-tad.es>
2017-11-28 14:34:11 +0100

    asd

---
+++ Assets/Scripts/SampleMind/RandomMind.cs
@@ -0,0 +1,21 @@
+﻿using Assets.Scripts.DataStructures;
+using UnityEngine;
+
+namespace Assets.Scripts.SampleMind
+{
+    public class RandomMind : AbstractPathMind {
+        public override void Repath()
+        {
+            
+        }
+
+        public override Locomotion.MoveDirection GetNextMove(BoardInfo boardInfo, CellInfo currentPos, CellInfo[] goals)
+        {
+            var val = Random.Range(0, 4);
+            if (val == 0) return Locomotion.MoveDirection.Up;
+            if (val == 1) return Locomotion.MoveDirection.Down;
+            if (val == 2) return Locomotion.MoveDirection.Left;
+            return Locomotion.MoveDirection.Right;
+        }
+    }
+}
