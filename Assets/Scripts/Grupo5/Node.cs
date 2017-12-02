ec09b2d0a1999a27ae189118013923102678ee64
Alberto <alberto.soutullo@hotmail.com>
2017-12-02 17:16:58 +0100

    -Node class added

---
+++ Assets/Scripts/Grupo5/Node.cs
@@ -0,0 +1,32 @@
+﻿using Assets.Scripts.Grupo5;
+using UnityEngine;
+
+
+namespace Assets.Scripts.Grupo5
+{
+
+    public class Node
+    {
+        private Node father { get; set; }
+        private CellInfo cell { get; set; }
+
+        //Constructor de nodo, donde deberíamos pasarle el padre y los datos, que sería la informacion
+        //de la celda en la que está el personaje
+        public Node(Node father, CellInfo cell)
+        {
+            this.cell = cell;
+            this.father = father;
+        }
+
+        //Método para expandir los nodos a partir del nodo en el que nos encontramos.
+        public List<Node> Expand()
+        {
+            board = GetComponent<>
+        }
+
+
+    }
+
+
+    
+}
---
+++ Assets/Scripts/Grupo5/OffLineSearch.cs
@@ -0,0 +1,22 @@
+﻿using Assets.Scripts.DataStructures;
+using UnityEngine;
+
+namespace Assets.Scripts.Grupo5
+{
+    public class OffLineSearch : AbstractPathMind
+    {
+
+
+        public override void Repath()
+        {
+
+        }
+
+
+        public override Locomotion.MoveDirection GetNextMove(BoardInfo boardInfo, CellInfo currentPos, CellInfo[] goals)
+        {
+            return Locomotion.MoveDirection.Right;
+           
+        }
+    }
+}
