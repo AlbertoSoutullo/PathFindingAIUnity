ef913b76a892157ad3e8f695a9e1d027dfe0208f
alberto.soutullo <alberto.soutullo@hotmail.com>
2017-12-14 20:09:37 +0100

    +Node class semi-ended +GetNextMove ended +sortDecendents in process +A* in process

--- Assets/Scripts/Grupo5/Node.cs
+++ Assets/Scripts/Grupo5/Node.cs
@@ -1,5 +1,7 @@
-﻿using Assets.Scripts.Grupo5;
+﻿using Assets.Scripts.DataStructures;
+using Assets.Scripts.Grupo5;
 using UnityEngine;
+using System.Collections.Generic;
 
 
 namespace Assets.Scripts.Grupo5
@@ -8,7 +10,19 @@ namespace Assets.Scripts.Grupo5
     public class Node
     {
         private Node father { get; set; }
-        private CellInfo cell { get; set; }
+        private CellInfo cell;
+        private Locomotion.MoveDirection movement;
+
+        public CellInfo getCell()
+        {
+            return cell;
+        }
+
+        public Locomotion.MoveDirection getMovement()
+        {
+            return movement;
+        }
+
 
         //Constructor de nodo, donde deberíamos pasarle el padre y los datos, que sería la informacion
         //de la celda en la que está el personaje
@@ -16,17 +30,46 @@ namespace Assets.Scripts.Grupo5
         {
             this.cell = cell;
             this.father = father;
+            this.movement = getDirection();
         }
 
         //Método para expandir los nodos a partir del nodo en el que nos encontramos.
-        public List<Node> Expand()
+        public List<Node> Expand(BoardInfo boardInfo)
         {
-            board = GetComponent<>
+            List<CellInfo> cells = new List<CellInfo>();
+            List<Node> nodes = new List<Node>();
+
+            for (int i = 0; i < 4; i++)
+            {
+                cells.Add(this.cell.WalkableNeighbours(boardInfo)[i]);
+                if (cells[i] != father.getCell())
+                {
+
+                    Node aux = new Node(this, cells[i]);
+                    nodes.Add(aux);
+                }
+            }
+            return nodes;
         }
 
+        public Locomotion.MoveDirection  getDirection()
+        {
+            int column = this.cell.ColumnId - father.cell.ColumnId;
+            int row    = this.cell.RowId - father.cell.RowId;
+
+            if (column == 0)
+            {
+                if (row > 0) return Locomotion.MoveDirection.Up;
+                else return Locomotion.MoveDirection.Down;
+            }
+            else 
+            {
+                if (column > 0) return Locomotion.MoveDirection.Left;
+                else return Locomotion.MoveDirection.Right;
+            }
+
+        }
 
     }
-
-
     
 }
--- Assets/Scripts/Grupo5/OffLineSearch.cs
+++ Assets/Scripts/Grupo5/OffLineSearch.cs
@@ -1,11 +1,39 @@
-﻿using Assets.Scripts.DataStructures;
+﻿using Assets.Scripts.SampleMind;
+using Assets.Scripts.DataStructures;
 using UnityEngine;
+using System.Collections.Generic;
+using System;
 
 namespace Assets.Scripts.Grupo5
 {
     public class OffLineSearch : AbstractPathMind
     {
 
+        private Stack<Locomotion.MoveDirection> movements = null;
+
+
+        /*Creamos*/
+        private bool aStarAlgorithm(Node actualNode, BoardInfo boardInfo, CellInfo[] goals)
+        {
+            List<Node> decendents = new List<Node>();
+
+            for (int i = 0; i < goals.Length; i++)
+            {
+                if (actualNode.getCell() == goals[i])
+                {
+                    this.movements.Push(actualNode.getMovement());
+                    return true;
+                }
+            }
+
+            decendents = actualNode.Expand(boardInfo);
+            //Ordenamos
+            decendents = sortDecendents(decendents,goals);
+
+            if (aStarAlgorithm())
+
+            
+        }
 
         public override void Repath()
         {
@@ -15,8 +43,39 @@ namespace Assets.Scripts.Grupo5
 
         public override Locomotion.MoveDirection GetNextMove(BoardInfo boardInfo, CellInfo currentPos, CellInfo[] goals)
         {
-            return Locomotion.MoveDirection.Right;
-           
+
+            if (this.movements == null)
+            {
+                Node firstNode = new Node(null, currentPos);
+                this.movements = new Stack<Locomotion.MoveDirection>();
+                this.movements = aStarAlgorithm(firstNode, boardInfo, goals);
+            }
+            if (this.movements.Count == 0)
+            {
+                return Locomotion.MoveDirection.None;
+            }
+            else
+            {
+                return this.movements.Pop();
+                
+            }
+        }
+
+        //Metodo para ordenar el array de nodos.
+        private List<Node> sortDecendents(List<Node> Nodes, CellInfo[] goal)
+
+        {
+            int[] numeros = new int[Nodes.Count];
+            for(int i=0; i< Nodes.Count; i++)
+            {
+                int column = Math.Abs(Nodes[i].getCell().ColumnId - goal[0].ColumnId);
+                int row = Math.Abs(Nodes[i].getCell().RowId - goal[0].RowId);
+                int dist = column + row;
+                numeros[i] = dist;
+            }
+
+            
+            return null;
         }
     }
 }
