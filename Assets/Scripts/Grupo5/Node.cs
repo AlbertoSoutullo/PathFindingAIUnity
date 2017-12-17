16966c232f1debb4ab5d9a8a470742dbb7fbd65e
Alberto <alberto.soutullo@hotmail.com>
2017-12-17 21:18:55 +0100

    +New A* implementation +Modifications to Node class (distance) +New mehod for sort nodes (using distance and cost)

--- Assets/Scripts/Grupo5/Node.cs
+++ Assets/Scripts/Grupo5/Node.cs
@@ -2,16 +2,21 @@
 using Assets.Scripts.Grupo5;
 using UnityEngine;
 using System.Collections.Generic;
-
+using System;
 
 namespace Assets.Scripts.Grupo5
 {
 
     public class Node
     {
+        //En los nodos guardamos la dirección del padre, la información de la celda, y el movimiento que
+        //deberíamos tomar para llegar a goal.
+        //Esta bíen planteado tener que cambiar la clase nodo de la práctica 1 a la 2?
         private Node father { get; set; }
         private CellInfo cell;
         private Locomotion.MoveDirection movement;
+        private int distance;
+        private static int numberNodes = 0;
 
         public CellInfo getCell()
         {
@@ -26,15 +31,18 @@ namespace Assets.Scripts.Grupo5
 
         //Constructor de nodo, donde deberíamos pasarle el padre y los datos, que sería la informacion
         //de la celda en la que está el personaje
-        public Node(Node father, CellInfo cell)
+        public Node(Node father, CellInfo cell, CellInfo[] goals)
         {
+            numberNodes++;
             this.cell = cell;
             this.father = father;
             this.movement = getDirection();
+            this.distance = calculateDistance(goals);
+            Debug.Log("Node " + numberNodes + "created. \n");
         }
 
         //Método para expandir los nodos a partir del nodo en el que nos encontramos.
-        public List<Node> Expand(BoardInfo boardInfo)
+        public List<Node> Expand(BoardInfo boardInfo, CellInfo[] goals)
         {
             List<CellInfo> cells = new List<CellInfo>();
             List<Node> nodes = new List<Node>();
@@ -44,30 +52,51 @@ namespace Assets.Scripts.Grupo5
                 cells.Add(this.cell.WalkableNeighbours(boardInfo)[i]);
                 if (cells[i] != father.getCell())
                 {
-
-                    Node aux = new Node(this, cells[i]);
+                    Node aux = new Node(this, cells[i], goals);
                     nodes.Add(aux);
                 }
             }
             return nodes;
         }
 
-        public Locomotion.MoveDirection  getDirection()
+        public Locomotion.MoveDirection getDirection()
         {
-            int column = this.cell.ColumnId - father.cell.ColumnId;
-            int row    = this.cell.RowId - father.cell.RowId;
-
-            if (column == 0)
+            //Si el padre es null, osea, si es el primer nodo, no tendrá movimiento.
+            if (this.father == null)
             {
-                if (row > 0) return Locomotion.MoveDirection.Up;
-                else return Locomotion.MoveDirection.Down;
+                return Locomotion.MoveDirection.None;
             }
-            else 
+            else
             {
-                if (column > 0) return Locomotion.MoveDirection.Left;
-                else return Locomotion.MoveDirection.Right;
-            }
+                int column = this.cell.ColumnId - father.cell.ColumnId;
+                int row = this.cell.RowId - father.cell.RowId;
 
+                if (column == 0)
+                {
+                    if (row > 0) return Locomotion.MoveDirection.Up;
+                    else return Locomotion.MoveDirection.Down;
+                }
+                else
+                {
+                    if (column > 0) return Locomotion.MoveDirection.Left;
+                    else return Locomotion.MoveDirection.Right;
+                }
+            }
+        }
+
+        //Calculamos el coste de este nodo para poder usarlo en la comparación del sort.
+        public int calculateDistance(CellInfo[] goals)
+        {
+            int column = Math.Abs(this.getCell().ColumnId - goals[0].ColumnId);
+            int row = Math.Abs(this.getCell().RowId - goals[0].RowId);
+            int dist = column + row;
+
+            return dist;
+        }
+
+        public int getDistance()
+        {
+            return this.distance;
         }
 
     }
--- Assets/Scripts/Grupo5/OffLineSearch.cs
+++ Assets/Scripts/Grupo5/OffLineSearch.cs
@@ -1,5 +1,6 @@
 ﻿using Assets.Scripts.SampleMind;
 using Assets.Scripts.DataStructures;
+using Assets.Scripts.Grupo5;
 using UnityEngine;
 using System.Collections.Generic;
 using System;
@@ -9,31 +10,72 @@ namespace Assets.Scripts.Grupo5
     public class OffLineSearch : AbstractPathMind
     {
 
+        //Lista de Movimientos que va a devolver nuestro algoritmo
         private Stack<Locomotion.MoveDirection> movements = null;
+        private List<Node> nodes = null; //abierta
 
 
-        /*Creamos*/
-        private bool aStarAlgorithm(Node actualNode, BoardInfo boardInfo, CellInfo[] goals)
+        private bool test(List<Node> nodesToExpand, BoardInfo boardInfo, CellInfo[] goals)
         {
-            List<Node> decendents = new List<Node>();
-
-            for (int i = 0; i < goals.Length; i++)
+            while (true) // ??
             {
-                if (actualNode.getCell() == goals[i])
+                //Si ya no quedan nodos para expandir, no hemos encontrado solucion
+                if (nodesToExpand == null)
                 {
-                    this.movements.Push(actualNode.getMovement());
-                    return true;
+                    Debug.Log("Solution not found. \n");
+                    return false;
                 }
+
+                //Pillamos el nodo que toca expandir, y lo sacamos de la lista
+                Node actualNode = nodesToExpand[0];
+                nodesToExpand.Remove(actualNode);
+                this.movements.Push(actualNode.getMovement());
+
+                //si ese nodo es goal, hemos acabado
+                for (int i = 0; i < goals.Length; i++)
+                {
+                    if (actualNode.getCell() == goals[i])
+                    {
+                        return true;
+                    }
+                }
+
+                //Expandimos los sucesores de este nodo (expand ya hace que esos sucesores apunten al padre)
+                List<Node> sucessors = new List<Node>();
+                sucessors = actualNode.Expand(boardInfo, goals);
+                //Añadimos los sucesores a los nodos que tenemos que espandir
+                nodesToExpand.AddRange(sucessors);
+
+                //Reordenamos los nodos
+                nodesToExpand.Sort(compareNodesByDistance);
             }
 
-            decendents = actualNode.Expand(boardInfo);
-            //Ordenamos
-            decendents = sortDecendents(decendents,goals);
-
-            if (aStarAlgorithm())
-
-            
         }
+
+        //NO SE USA
+        //Metodo A* recursivo, donde le pasamos el nodo actual, la información del tablero y los goals.
+        //private bool aStarAlgorithm(Node actualNode, BoardInfo boardInfo, CellInfo[] goals)
+        //{
+        //    //Lista para guardar los descendientes
+        //    List<Node> decendents = new List<Node>();
+
+        //    //Primero miramos si ya estamos en una salida, y si lo estamos no devolvemos movimientos.
+        //    for (int i = 0; i < goals.Length; i++)
+        //    {
+        //        if (actualNode.getCell() == goals[i])
+        //        {
+        //            this.movements.Push(actualNode.getMovement());
+        //            return true;
+        //        }
+        //    }
+
+        //    //Sacamos los descendientes
+        //    decendents = actualNode.Expand(boardInfo);
+        //    //Ordenamos
+        //    decendents = sortDecendents(decendents, goals);
+
+        //    //
+        //}
 
         public override void Repath()
         {
@@ -43,12 +85,20 @@ namespace Assets.Scripts.Grupo5
 
         public override Locomotion.MoveDirection GetNextMove(BoardInfo boardInfo, CellInfo currentPos, CellInfo[] goals)
         {
-
             if (this.movements == null)
             {
-                Node firstNode = new Node(null, currentPos);
                 this.movements = new Stack<Locomotion.MoveDirection>();
-                this.movements = aStarAlgorithm(firstNode, boardInfo, goals);
+                this.nodes = new List<Node>();
+                Node firstNode = new Node(null, currentPos, goals);
+                this.nodes.Add(firstNode);
+
+                bool encontrado = test(this.nodes, boardInfo, goals);
+                if (!encontrado)
+                {
+                    print("Goal no encontrado");
+                }
+                //Mirar si no ha encontrado goal.
+                //aStarAlgorithm(firstNode, boardInfo, goals);
             }
             if (this.movements.Count == 0)
             {
@@ -57,25 +107,72 @@ namespace Assets.Scripts.Grupo5
             else
             {
                 return this.movements.Pop();
-                
             }
         }
 
-        //Metodo para ordenar el array de nodos.
-        private List<Node> sortDecendents(List<Node> Nodes, CellInfo[] goal)
+        ////NO SE USA
+        ////Metodo para ordenar el array de nodos. (Heurística)
+        //private List<Node> sortDecendents(List<Node> Nodes, CellInfo[] goal)
+        //{
+        //    int[] numeros = new int[Nodes.Count];
+        //    for(int i=0; i< Nodes.Count; i++)
+        //    {
+        //        int column = Math.Abs(Nodes[i].getCell().ColumnId - goal[0].ColumnId);
+        //        int row = Math.Abs(Nodes[i].getCell().RowId - goal[0].RowId);
+        //        int dist = column + row;
+        //        numeros[i] = dist;
+        //    }  
+        //    return null;
+        //}
 
+        //Método comparatorio que usaremos en el sort.
+        private int compareNodesByDistance(Node x, Node y)
         {
-            int[] numeros = new int[Nodes.Count];
-            for(int i=0; i< Nodes.Count; i++)
+            if (x == null)
             {
-                int column = Math.Abs(Nodes[i].getCell().ColumnId - goal[0].ColumnId);
-                int row = Math.Abs(Nodes[i].getCell().RowId - goal[0].RowId);
-                int dist = column + row;
-                numeros[i] = dist;
+                if (y == null)
+                {
+                    return 0; //Si ambos son null, ambos son iguales
+                }
+                else
+                {
+                    return -1; //Si x es null pero y no, y es mas grande
+                }
             }
+            else //Si x no es null
+            {
+                if (y == null)  // e y es null, x es mas grande
+                {
+                    return 1;
+                }
+                else //Si ni x ni y son null, los comparamos, primero por coste, despues por distancia
+                {
+                    if (x.getCell().WalkCost == y.getCell().WalkCost) //Si el coste es el mismo (como debería ser en esta práctica)
+                    {
+                        if (x.getDistance() >= y.getDistance()) //si X está mas lejos que y, x es mayor
+                        {
+                            return 1;
+                        }
+                        else
+                        {
+                            return -1; //Si no, y es mayor.
+                        }
+                    }
+                    else
+                    {
+                        if (x.getCell().WalkCost > y.getCell().WalkCost) //Si el coste de X es mayor, x es mayor.
+                        {
+                            return 1;
+                        }
+                        else
+                        {
+                            return -1;
+                        }
+                    }
 
-            
-            return null;
+                }
+
+            }
         }
     }
 }
